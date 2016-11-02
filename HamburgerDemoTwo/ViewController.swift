b647e6717e95f54e871902b29839a305f71d5152
Charlie Hieger <charlie@codepath.com>
2016-11-02 09:32:25 -0700

    Initial Hamburger implementation

--- HamburgerDemoTwo/ViewController.swift
+++ HamburgerDemoTwo/ViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backContainerView: UIView!
+
+    @IBOutlet weak var frontContainerView: UIView!
+
+    var greenView: UIView!
+    var redView: UIView!
+
+    var main = UIStoryboard(name: "Main", bundle: nil)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let greenViewController = main.instantiateViewController(withIdentifier: "GreenViewController")
+        let redViewController = main.instantiateViewController(withIdentifier: "RedViewController")
+        greenViewController.view.frame = backContainerView.bounds
+        backContainerView.addSubview(redViewController.view)
+        redViewController.view.frame = frontContainerView.bounds
+        frontContainerView.addSubview(greenViewController.view)
+
+
     }
+
+    @IBAction func didPanTopView(_ sender: UIPanGestureRecognizer) {
+
+        let translation = sender.translation(in: view)
+
+        frontContainerView.transform = frontContainerView.transform.translatedBy(x: translation.x, y: 0)
+        sender.setTranslation(CGPoint.zero, in: view)
+
+        print("Hello")
+    }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
